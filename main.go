f6138c9944fd157b567a4f4d3b8c70af35f9ed3a
Soozism <a.soozism@gmail.com>
2024-07-25 15:44:34 +0330

    add new way

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
 	"os"
 )
@@ -12,8 +13,11 @@ func main() {
 		fmt.Println("Error: ", err)
 		os.Exit(1)
 	}
-
+	// first way to show body
 	b := make([]byte, 99999)
 	resp.Body.Read(b)
 	fmt.Println(string(b))
+	
+	// seccond way to show body
+	io.Copy(os.Stdout, resp.Body)
 }
